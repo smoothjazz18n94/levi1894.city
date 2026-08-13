887ad12556ffa3d8f3a53e71bb1f947ba29fa85b
smoothjazz18n94 <gorjessbrown@gmail.com>
2026-08-13 19:09:59 +0000

    Update script.js

--- Personal/script.js
+++ Personal/script.js
@@ -469,21 +469,6 @@ document.addEventListener("DOMContentLoaded", () => {
   initTyping();
 });
 
-/* ===================================
-   SCROLL REVEAL
-=================================== */
-
-const revealElements = document.querySelectorAll(".reveal");
-
-const revealObserver = new IntersectionObserver((entries)=>{
-  entries.forEach(entry=>{
-    if(entry.isIntersecting){
-      entry.target.classList.add("visible");
-    }
-  });
-},{
-  threshold:0.15
-});
 
 revealElements.forEach(el=>{
   revealObserver.observe(el);
